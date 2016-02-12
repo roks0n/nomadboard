fd274d0940befeb31673ea0616c49d654c126d57
Rok Haluzan <haluzan.rok@gmail.com>
2016-02-12 20:47:20 +0000

    fix bash script

--- run_spider.sh
+++ run_spider.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd "${BASH_SOURCE%/*}" || exit  # cd into the bundle and use relative paths
+cd $(dirname $(readlink -f $0))  # cd into the bundle and use relative paths
 spider_slug="$1"
 DJANGO_CMD="run_spider --scraper_slug $spider_slug" make django
